624fd347fa3d9f98b416fe0443e01b365ff87602
Arwin Marinta <arwinmarinta3@gmail.com>
2024-02-27 20:34:34 +0800

    first commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "",
+    database: "Koperasi_ITK",
+    host: "localhost",
+    dialect: "mysql",
+  },
+};
